8ec9572537e2fe42873b14bc835cef9a99cb166e
Sane Generous <karim2k11@gmail.com>
2024-11-28 16:48:15 +0100

    Route added for inventory service closes #3

--- src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -14,6 +14,12 @@ public class LocalHostRouteConfig {
                 .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                         .uri("http://localhost:8080")
                         .id("beer-service"))
+                .route(r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081")
+                        .id("Order-service"))
+                .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .uri("http://localhost:8082")
+                        .id("Inventory-service"))
                 .build();
     }
 }
